0a6d8e554525ff3d4a64081ef09d8f4ddcbe470b
Hubert Adamkowski <hubertadamkowskip56@gmail.com>
2024-09-15 20:28:30 +0200

    editing config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/frontend_recruitment_smart_it/', // Nazwa Twojego repozytorium
+  plugins: [react()],
+  base: '/frontend_recruitment_smart_it/', 
 });
